4b0cac8bd883a0df2e56c13dfa87c43d991141f7
BasileusRedRain <valyrian.reyne@gmail.com>
2019-05-21 18:02:18 +0200

    klient uzupełniony

--- Projekt2SK/src/rumba_main/Klient.java
+++ Projekt2SK/src/rumba_main/Klient.java
@@ -1,5 +1,61 @@
 package rumba_main;
 
-public class Klient {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Klient {
+	public static void main(String[] args) {
+		DatagramSocket socket = null;
+		DatagramPacket inPacket = null; //recieving packet
+		DatagramPacket outPacket = null; //sending packet
+		byte[] inBuf, outBuf;
+		String msg = null;
+		final int PORT = 50000;
+		Scanner src = new Scanner(System.in);
+		try {
+			InetAddress address = InetAddress.getByName("127.0.0.1");
+			socket = new DatagramSocket();
+			msg="";
+			
+			outBuf=msg.getBytes();
+			outPacket = new DatagramPacket(outBuf, 0, outBuf.length, address,PORT);
+			socket.send(outPacket);
+			
+			inBuf = new Byte[65535];
+			inPacket = new DatagramPacket(inBuf, inBuf.length);
+			socket.receive(inPacket);
+			
+			String data = new String(inPacket.getData(), 0, inPacket.getLength());
+			//Printing file list
+			System.out.println(data);
+			
+			//Send file name
+			String filename = src.nextLine();
+			outBuf = filename.getBytes();
+			outPacket = new DatagramPacket(outBuf, 0, outBuf.lengthm address,PORT);
+			socket.send(outPacket);
+			data=new String(inPacket.getData(), 0, inPacket.getLength());
+			if(data.endsWith("ERROR")) {
+				System.out.println("File does not exist");
+				socket.close();
+			}
+			else {
+				try {
+					BufferedReader pw = new BufferedReader(new OutputStreamWriter(new FileOutputStream(filename)));
+					pw.write(data);
+					pw.close();
+					System.out.println("FIle writing successful, closing socket");
+					socket.close();
+				}
+				catch(IOException ioe) {
+					System.out.println("File error\n");
+					socket.close();
+				}
+			}
+		}
+		catch(|Exception e) {
+			System.out.println("Network error, please try again.\n");
+		}
+	}
 }
